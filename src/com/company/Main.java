b480ef4fad8030774af4332704db7d4d1eee94cc
Vasil <vdvasilev04@gmail.com>
2020-02-28 17:39:39 +0200

    second part, added addition and subtraction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,46 @@ public class Main {
         printMatrix(firstMatrix);
         System.out.println("Желаете ли да създадете втора матрица? Ако желаете въведете 'да' или 'yes', ако не желаете въведете - 'не' или 'no'. При всеки вход различен от 'да' или 'yes' няма да бъде създадена нова матрица.");
         String response = input.nextLine();
-        if (response.equalsIgnoreCase("да") || response.equalsIgnoreCase("yes")){
+        if (response.equalsIgnoreCase("да") || response.equalsIgnoreCase("yes")) {
             int[][] secondMatrix = createMatrix(input);
             printMatrix(secondMatrix);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            System.out.println("Какво действие желаете да извършите:събиране;изваждане;умножение.");
+            String action = input.nextLine();
+            if (action.equalsIgnoreCase("събиране")) {
+                addition(firstMatrix, secondMatrix);
+            }
+            if (action.equalsIgnoreCase("изваждане")){
+                subtraction(firstMatrix, secondMatrix);
+            }
+        }
+    }
+
+    private static void subtraction(int[][] firstMatrix, int[][] secondMatrix) {
+        if (firstMatrix.length == secondMatrix.length && firstMatrix[0].length == secondMatrix[0].length) {
+            for (int i = 0; i < firstMatrix.length; i++) {
+                for (int j = 0; j < firstMatrix[0].length; j++) {
+                    System.out.print(firstMatrix[i][j] - secondMatrix[i][j]);
+                }
+                System.out.println();
+            }
+        }
+        else{
+            System.out.println("Двете матрици не може да бъдат събрани");
+        }
+    }
+
+    private static void addition(int[][] firstMatrix, int[][] secondMatrix) {
+        if (firstMatrix.length == secondMatrix.length && firstMatrix[0].length == secondMatrix[0].length) {
+            for (int i = 0; i < firstMatrix.length; i++) {
+                for (int j = 0; j < firstMatrix[0].length; j++) {
+                    System.out.print(firstMatrix[i][j] + secondMatrix[i][j]);
+                }
+                System.out.println();
+            }
+        }
+        else{
+            System.out.println("Двете матрици не може да бъдат събрани");
         }
     }
 
